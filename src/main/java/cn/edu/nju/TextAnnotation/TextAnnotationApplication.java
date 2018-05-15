1f014c99c0d48d561032aeaa85f8f5c8f94ef3aa
151250206 <151250206@smail.nju.edu.cn>
2018-05-15 23:24:30 +0800

    init

---
+++ src/main/java/cn/edu/nju/TextAnnotation/TextAnnotationApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju.TextAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnnotationApplication.class, args);
+	}
+}
