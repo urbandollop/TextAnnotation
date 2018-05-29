75d26eb09329a8811de0925666a0b23803cdcb54
tirainbar <39292727+tirainbar@users.noreply.github.com>
2018-05-29 14:53:45 +0800

    添加instrumentlist页面跳转judgeAnnotation页面

--- src/main/java/cn/edu/nju/TextAnnotation/controller/ShowAnnotationController.java
+++ src/main/java/cn/edu/nju/TextAnnotation/controller/ShowAnnotationController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,22 +18,22 @@ import java.util.List;
  * @date:2018/5/16
  */
 @Controller
-@RequestMapping("/judgeAnnotation")
+@RequestMapping("/judge/judgeAnnotation")
 public class ShowAnnotationController {
     @Autowired
     private FactService factService;
     @Autowired
     private StatuteService statuteService;
-    @RequestMapping(value = "/judge", method = RequestMethod.GET)
-    public String displayShowPlan(Model model) {
-        String instrumentid="006_376154.xml_ft2jl.xls_xsys";
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String displayShowPlan(Model model,@RequestParam(value = "iid", required = true) String instrumentid) {
+
         List<FactListBean> factListBeans=factService.getAllFactByInstrumentId(instrumentid);
         model.addAttribute("factListBeans", factListBeans);
 
         List<StatuteListBean> statuteListBeans=statuteService.getAllStatuteByStatuteid(instrumentid);
         model.addAttribute("statuteListBeans", statuteListBeans);
 
-        return "judge/judgeAnnotation";
+        return "/judge/judgeAnnotation";
     }
 
 }
