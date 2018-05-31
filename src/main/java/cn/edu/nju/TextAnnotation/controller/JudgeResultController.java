3a48287c3768122742cacd81562e3b56b973c782
cccyf <731767381@qq.com>
2018-05-31 11:38:58 +0800

    <feat>:<add some classes about judgement and modify the document>

---
+++ src/main/java/cn/edu/nju/TextAnnotation/controller/JudgeResultController.java
@@ -0,0 +1,4 @@
+package cn.edu.nju.TextAnnotation.controller;
+
+public class JudgeResultController {
+}
---
+++ src/main/java/cn/edu/nju/TextAnnotation/service/serviceImpl/JudgeResultServiceImpl.java
@@ -0,0 +1,31 @@
+package cn.edu.nju.TextAnnotation.service.serviceImpl;
+
+import cn.edu.nju.TextAnnotation.model.Judgement;
+import cn.edu.nju.TextAnnotation.repository.JudgementRepository;
+import cn.edu.nju.TextAnnotation.service.JudgeResultService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JudgeResultServiceImpl implements JudgeResultService {
+    @Autowired
+    private JudgementRepository judgementRepository;
+
+    @Override
+    public Map<Integer, Boolean> findJudgeResult(int projectId) {
+        Map<Integer, Boolean> userFinished = new HashMap<>();
+        List<Judgement> judgements = judgementRepository.findAllByProjectId(projectId);
+        for (Judgement j : judgements) {
+            int id = j.getUserId();
+            int res = j.getIsrelated();
+            if (res == -1) {
+                userFinished.put(id, false);
+            } else if (!userFinished.containsKey(id)) {
+                userFinished.put(id, true);
+            }
+        }
+        return userFinished;
+    }
+}
